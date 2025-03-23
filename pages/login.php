0780dadf0803a3072c81291450161d47cc5e3f0a
claudiaprt <prati.cld@gmail.com>
2025-03-23 15:18:00 +0100

    interfaccia utente login

--- pages/login.php
+++ pages/login.php
@@ -6,6 +6,31 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 </head>
 <body>
-    ciao
+<div class="row">
+        <div class="col-lg-6 col-12 px-5 py-5">
+            <img class="rounded img-fluid px-5 py-3" src="public/images/logo-ambulanza.png" alt="Logo Pronto Emergenza">
+            <img class="rounded img-fluid px-5 py-3" src="public/images/icona-ambulanza.png" alt="Icona Ambulanza">
+        </div>
+        <div class="col-lg-6 col-12 bg-warning min-vh-100 align-middle px-5 py-5 rounded">
+            <form action="#" method="POST" class="container-fluid row align-items-center justify-content-center" name="formLog" onsubmit="return loginJS(this)">
+                <div class="col-12">
+                    <label for="inputUsername" class="form-label">Username</label>
+                    <input type="text" id="inputUsername" placeholder="username" class="form-control"aria-label="Username">
+                </div>
+                <div class="col-12">
+                    <label for="inputPassword" class="form-label">Password</label>
+                    <input type="password" id="inputPassword" placeholder="password" class="form-control" aria-label="Password">
+                </div>
+                <div class="col-12"><h2 id="h2Log"></h2></div>
+                <div class="col-12 text-center">
+                    <button class="btn btn-primary" type="submit">ACCEDI</button>
+                </div>
+                <div class="col-12 text-center text-primary">
+                    <a href="ripristino" class="text">Password dimenticata??</a>
+                    <a href="registrazione" class"text">Registrati</a>
+                </div>
+            </form> 
+        </div>
+    </div>
 </body>
 </html>
