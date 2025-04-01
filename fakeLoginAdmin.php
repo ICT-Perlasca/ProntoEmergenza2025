8e1d8817b39a17cac7100add515bd692450eae97
Gallina Roberto <gallinar00@gmail.com>
2025-04-01 11:41:10 +0200

    Add image

--- fakeLoginAdmin.php
+++ fakeLoginAdmin.php
@@ -6,6 +6,8 @@ $_SESSION['nome'] = "Mario";
 $_SESSION['cognome'] = "Rossi";
 $_SESSION['status'] = 'volontario';
 $_SESSION['tipoUtente'] = 'admin';
+$_SESSION['image'] = "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1";
+
 
 
 echo "Loggato come amministratore Mario Rossi (volontario)";
