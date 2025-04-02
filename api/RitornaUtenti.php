f2876c37c48abafdb3d3ad6a086d0b3ee09cb017
utente <ilariomorandi05@outlook.com>
2025-04-02 10:39:02 +0200

    L'api per la RitornoUtente creata

---
+++ api/RitornaUtenti.php
@@ -0,0 +1,36 @@
+<?php
+
+function API_RitornaUtenti($get, $post, $session){
+    if (!isset($session['tipoUtente']) || $session['tipoUtente'] != "admin"){
+        header("HTTP/1.1 403 forbidden");
+        return [];
+    }
+    $sql = "SELECT * from utenti ORDER BY cognome,nome;";
+    $valori = [];
+    $tipi = [];
+    $risposta = ElaboraQuery($sql, $valori, $tipi);
+    return $risposta;
+}
+
+
+function ElaboraQuery($strquery, $valori, $tipi) {
+    include ("dsn.php"); 
+    $ris=[]; 
+    try{ 
+        $conn=new PDO($dsn,$username,$password); 
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 
+        $stm=$conn->prepare($strquery); 
+        for($i=0;$i<count($valori);$i++){ 
+            $stm->bindParam($i+1,$valori[$i],$tipi[$i]); 
+        } 
+        $stm->execute(); 
+        $ris = $stm->fetchAll();
+
+        $conn=null; 
+    }catch(Exception $e){ 
+        $ris['error']=$e->getMessage(); 
+    } 
+    return $ris; 
+}
+
+?>
