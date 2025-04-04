f4260693d8769bb3e50c91b910b236c6a76a140d
utente <ilariomorandi05@outlook.com>
2025-04-04 08:43:38 +0200

    creazione codice GetMezzo e controllo del giusto funzionamento del codice

---
+++ api/GetMezzo.php
@@ -0,0 +1,35 @@
+<?php
+
+function API_GetMezzo($get, $post, $session){
+    if(!isset($_POST['idMezzo']) || $session['tipoUtente'] != "admin"){
+        header("HTTP/1.1 403 forbidden");
+        return [];
+    }else{
+        $sql = "SELECT * from mezzi WHERE idMezzo=?;";
+        $valori = [$_GET['idMezzo']];
+        $tipi = [PDO::PARAM_STR];
+        $risposta = ElaboraQuery($sql, $valori, $tipi);
+        return $risposta;
+    }
+}
+
+
+function ElaboraQuery($strquery, $valori, $tipi) {
+    include ("dsn.php"); /*file temp per provare le query, serve per settare la classe PDO*/
+    $ris=[]; 
+    try{ 
+        $conn=new PDO($dsn,$username,$password); 
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 
+        $stm=$conn->prepare($strquery); 
+        for($i=0;$i<count($valori);$i++){ 
+            $stm->bindParam($i+1,$valori[$i],$tipi[$i]); 
+        } 
+        $stm->execute(); 
+        $ris = $stm->fetchAll();
+
+        $conn=null; 
+    }catch(Exception $e){ 
+        $ris['error']=$e->getMessage(); 
+    } 
+    return $ris; 
+}
