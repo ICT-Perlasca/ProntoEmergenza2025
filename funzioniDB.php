faeca52f7ddab3bb5bb5f68f2604e93d1fca71b6
claudiaprt <prati.cld@gmail.com>
2025-04-12 16:09:57 +0200

    sistemata funzione x query sul DB

--- funzioniDB.php
+++ funzioniDB.php
@@ -1,18 +1,27 @@
 <?php
-require_once ""
-function api_querySelect($strquery,$valori, $tipi){
+require_once "globals.php";
+function api_query($strquery,$valori, $tipi){
     global $DSN,$USERDB,$PSWDB;
     $ris=[];
     try{
         $conn=new PDO($DSN,$USERDB,$PSWDB);
         $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         $stm=$conn->prepare($strquery);
-        for($i=0;$i<count($valori);$i++){
-            $stm->bindParam($i+1,$valori[$i],$tipi[$i]);
+        if (!empty($valori)) {
+            for($i=0;$i<count($valori);$i++){
+                $stm->bindParam($i+1,$valori[$i],$tipi[$i]);
+            }
         }
         $stm->execute();
-
-        $ris=$stm->fetchAll(PDO::FETCH_NAMED);
+        if (stripos($strquery, "INSERT") === 0 || stripos($strquery, "UPDATE") === 0 || stripos($strquery, "DELETE") === 0) {
+            $ris= $stm->rowCount();
+        } elseif (stripos($strquery, "SELECT") === 0) {
+            $ris= $stm->fetchAll(PDO::FETCH_NAMED);
+        } else {
+            // Tipo di query non riconosciuto (errrore di battitura ecc..)
+            $ris['error']= "tipo di query non riconosciuto";
+         }
+        //ris=$stm->fetchAll(PDO::FETCH_NAMED);
         $conn=null;
     }
     catch(Exception $e){
@@ -20,4 +29,5 @@ function api_querySelect($strquery,$valori, $tipi){
     }
     return $ris;
 }
+ 
 ?>
--- pages/home.php
+++ pages/home.php
@@ -2,6 +2,9 @@
 session_start();
 
 if(!isset($_SESSION['idUtente'])){
+    header("location:/ProntoEmergenza2025/login");
+}
+else {
 ?>
 <html>
         <head>
