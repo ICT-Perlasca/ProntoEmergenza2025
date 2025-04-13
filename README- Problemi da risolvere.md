9fa5d535b88ad6c2eef7d1f13d383bfc40209f81
claudiaprt <prati.cld@gmail.com>
2025-04-13 14:36:11 +0200

    rionosce utente ma non passa a home

--- README- Problemi da risolvere.md
+++
@@ -1,1 +0,0 @@
-individuare come un bottone adatti la forma (da quadrata a circolare e viceversa) in base alla dimensione della finestra
--- pages/home.php
+++ pages/home.php
@@ -2,9 +2,8 @@
 session_start();
 
 if(!isset($_SESSION['idUtente'])){
-    header("location:/ProntoEmergenza2025/login");
-}
-else {
+    header("Location: login");
+}else{
 ?>
 <html>
         <head>
@@ -12,19 +11,22 @@ else {
             <link href="./public/css/bootstrap.min.css" rel="stylesheet"/>
             <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         </head>
         <body>
 <?php
 require_once ("./components/Header/header.php");
 require_once ("./components/SimpleComponent/comp.php");
 require_once ("./api/elencoComunicazioni.php");
+require_once ("./components/Footer/footer.php");
 
-echo COMP_header();
+echo COMP_header($_SESSION);
 
 foreach(API_elencoComunicazioni([],[], $_SESSION) as $c) {
     echo COMP_simpleComponent($c['id']);
 }
 
+echo COMP_Footer();
 ?>
 </body>
 </html>
