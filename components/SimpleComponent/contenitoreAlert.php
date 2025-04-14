c0038fbc2e3f1d82557994208001c735d21220ce
ElKev05 <rumikevin2005@gmail.com>
2025-04-14 13:32:39 +0200

    contenitore html alert

--- components/SimpleComponent/contenitoreAlert.php
+++ components/SimpleComponent/contenitoreAlert.php
@@ -6,7 +6,6 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
-        <button onclick="aggiungiAlert('ciao', 'contenitore');"> TEST </button>
         <div id="contenitore" class="mt-3">
                     
         </div>
