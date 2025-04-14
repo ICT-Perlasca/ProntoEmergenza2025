4d1fa2328cf15d2242ce9591879b043b5ce0ae9d
unknown <MANUEL.TONONI>
2025-04-14 13:43:42 +0200

    aggiunta funzione bottoni

--- components/SimpleComponent/COMP_Buttons.php
+++ components/SimpleComponent/COMP_Buttons.php
@@ -1,15 +1,15 @@
 <?php
-function COMP_Button($icon, $label) {
+function COMP_Button($icon, $label, $link) {
     return '
     <div class="col-sm-6 col-md-3 text-center">
-        <a href="' . $label . '" 
-      class="d-sm-none d-md-inline-flex btn btn-outline-primary rounded-circle flex-column align-items-center justify-content-center"
-      style="width: 100px; height: 100px;">
+        <a href="' . $link . '" 
+			class="d-sm-none d-md-inline-flex btn btn-outline-primary rounded-circle flex-column align-items-center justify-content-center"
+			style="width: 100px; height: 100px;">
             <i class="bi bi-' . $icon . '"></i>
             <p>' . $label . '</p>
         </a>
 
-        <a href="' . $label . '" 
+        <a href="' . $link . '" 
            class="d-sm-inline-flex d-md-none btn btn-outline-primary w-100 flex-column align-items-center justify-content-center">
             <i class="bi bi-' . $icon . '"></i>
             <p>' . $label . '</p>
@@ -17,4 +17,3 @@ function COMP_Button($icon, $label) {
     </div>';
 }
 ?>
- 
