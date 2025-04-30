7e4c608fa3f5b3753b95ce3c681e82017a2914c1
unknown <MANUEL.PISANO>
2025-04-30 10:08:40 +0200

    Add searchbar

---
+++ components/SimpleComponent/searchBar.php
@@ -0,0 +1,7 @@
+<?php
+	function CMP_SearchBar(){
+		return "
+		<input class=form-control type=text name='cognomeNome' placeholder='Inserisci Nome e Cognome' onInput='filtraUtenti(this)'>
+		<script src='.\public\js\searchBar.js'></script>";
+	}
+?>	
