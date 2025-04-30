b78239e84851dc80d7d53b986e516f5a44fc8287
unknown <MATTIA.PASINETTI>
2025-04-30 09:30:13 +0200

    creazione funzione invio email con annessa prova

--- pages/invioemail.php
+++ pages/invioemail.php
@@ -1,10 +1,24 @@
 <?php
-$htmlContent = file_get_contents('emailfont.html');
-$to = 'alessandrolombardi860@gmail.com';
-$subject = 'pronto emergenza';
-$headers = "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-$headers .= "From: mattiapasinetti05@gmail.com\r\n";
-mail($to, $subject, $htmlContent, $headers);
-//mail('email@destinatario.it', 'oggetto della mail', 'contenuto del messaggio');
-?>+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $titoloUtente = $_POST["titolo"];
+    $testoUtente = $_POST["testo"];
+    $to = $_POST["destinatario"];
+    $subject = $_POST["oggetto"];
+
+    $htmlContent = file_get_contents('emailfont.html');
+
+    $htmlContent = str_replace(['TITOLO', 'testo'], [$titoloUtente, $testoUtente], $htmlContent);
+
+    $headers = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    $headers .= "From: mattiapasinetti05@gmail.com\r\n";
+
+    if (mail($to, $subject, $htmlContent, $headers)) {
+        echo "Email inviata con successo a !".$to ;
+    } else {
+        echo "Errore nell'invio dell'email.";
+    }
+} else {
+    echo "Accesso non autorizzato.";
+}
+?>
