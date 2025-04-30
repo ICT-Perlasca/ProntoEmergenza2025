53accde982e8d86bb5d49bc3ba322966198e75d0
ManuelVersini <versinimanuel2005@gmail.com>
2025-04-30 09:33:35 +0200

    Update API_upload_img.php

--- api/API_upload_img.php
+++ api/API_upload_img.php
@@ -1,25 +1,41 @@
 <?php
-function upload($foto) 
+function upload($get, $post, $session)
 {
+    if (!isset($_FILES['file'])) 
+    {
+        return false; 
+    }
+
+    $foto = $_FILES['file'];
     $estensioni = ['jpg', 'jpeg', 'png'];
-    $dimensioneMassima = 3 * 1024 * 1024; //dimensione massima 5 MB
-    $estensione = strtolower(pathinfo($foto['name'], PATHINFO_EXTENSION));
+    $dimensioneMassima = 3 * 1024 * 1024; // 3MB
     $percorso = "";
     $result = false;
 
-    if (in_array($estensione, $estensioni)) {
-        if ($foto['size'] <= $dimensioneMassima) {
-            if (is_uploaded_file($foto['tmp_name'])) {
-                $ts = new DateTime("now");
-                $cartella = '../uploads/images/';
-                $nomeFile = $_SESSION['username'] . "_" . $ts->getTimestamp() . "." . $estensione;
-                $percorso = $cartella . $nomeFile;
-
-                if (move_uploaded_file($foto['tmp_name'], $percorso)) {
-                    $result = $nomeFile;
-                }
-            }
-        }
+    $estensione = strtolower(pathinfo($foto['name'], PATHINFO_EXTENSION));
+    if (!in_array($estensione, $estensioni)) 
+    {
+        return false; 
+    }
+    if ($foto['size'] > $dimensioneMassima) 
+    {
+        return false; 
+    }
+    if (!is_uploaded_file($foto['tmp_name'])) 
+    {
+        return false; 
+    }
+    $ts = new DateTime("now");
+    $cartella = '../uploads/images/';
+    if (!file_exists($cartella)) 
+    {
+        mkdir($cartella, 0755, true);
+    }
+    $nomeFile = $session['username'] . "_" . $ts->getTimestamp() . "." . $estensione;
+    $percorso = $cartella . $nomeFile;
+    if (move_uploaded_file($foto['tmp_name'], $percorso)) 
+    {
+        $result = $nomeFile;
     }
 
     return $result;
