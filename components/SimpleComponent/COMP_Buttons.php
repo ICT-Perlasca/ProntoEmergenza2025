4fde8999665b4b258c7b03090ad4e5b62c14a0a3
DavideTogni06 <davide.togni2608@gmail.com>
2025-05-03 08:46:22 +0200

    Add files via upload

--- components/SimpleComponent/COMP_Buttons.php
+++ components/SimpleComponent/COMP_Buttons.php
@@ -3,17 +3,20 @@ function COMP_Button($icon, $label, $link) {
     return '
     <div class="col-sm-6 col-md-3 text-center">
         <a href="' . $link . '" 
-			class="d-sm-none d-md-inline-flex btn btn-outline-primary rounded-circle flex-column align-items-center justify-content-center"
+			class="d-none d-md-inline-flex btn btn-outline-primary rounded-circle flex-column align-items-center justify-content-center"
 			style="width: 100px; height: 100px;">
             <i class="bi bi-' . $icon . '"></i>
             <p>' . $label . '</p>
         </a>
 
         <a href="' . $link . '" 
-           class="d-sm-inline-flex d-md-none btn btn-outline-primary w-100 flex-column align-items-center justify-content-center">
-            <i class="bi bi-' . $icon . '"></i>
-            <p>' . $label . '</p>
+        class="d-inline-flex d-md-none btn btn-outline-primary w-100 flex-row align-items-center justify-content-center py-3 my-2">
+            <span class="d-flex align-items-center">
+                <i class="bi bi-' . $icon . ' mx-2"></i>
+                <span>' . $label . '</span>
+            </span>
         </a>
+
     </div>';
 }
 ?>
