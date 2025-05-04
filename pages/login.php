8b29a7fdaba7c791dea4b45b384f34e50254de78
claudiaprt <prati.cld@gmail.com>
2025-05-04 22:50:32 +0200

    sistemazione favincon

--- pages/login.php
+++ pages/login.php
@@ -37,6 +37,7 @@ else {
         <base href="./" />
         <base href="./" />
         <link href="./public/css/bootstrap.min.css" rel="stylesheet"/>
+        <link rel='icon' type='image/x-icon' href='/prontoemergenza2025/public/images/favicon.png'>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
         
