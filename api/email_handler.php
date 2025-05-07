6f124dd7845deac2595596d99c30e587434cf204
claudiaprt <prati.cld@gmail.com>
2025-05-07 13:18:03 +0200

    creazione messaggi default per email

--- api/email_handler.php
+++ api/email_handler.php
@@ -6,27 +6,27 @@ essere sostituiti prima di essere passati come parametri alla funzione inviaEmai
 */
 // formato della email di validazione della email dell'utente necessaria in caso di nuovo utente
 //sostituire {nomeUtente} e {link} con i valori coerenti
-$validazione_email=[
+$validazioneEmail=[
     "titolo"=>"ciao {nomeUtente}",
-    "testo"="Ti sei registrato come nuovo utente su ProntoEmergenza!
+    "testo"=>"Ti sei registrato come nuovo utente su ProntoEmergenza!<br>
 Per completare la registrazione e accedere a tutti i vantaggi, ti preghiamo di confermare il tuo indirizzo email cliccando sul link qui sotto:
-{link}.
-Se il link non funziona, puoi copiarlo e incollarlo nel tuo browser.
-Se non hai effettuato questa registrazione, ignora questo messaggio.
-Grazie ancora e benvenuto a nome di tutta l'associazione!
-Cordiali saluti,
+{link}.<br>
+Se il link non funziona, puoi copiarlo e incollarlo nel tuo browser.<br>
+Se non hai effettuato questa registrazione, ignora questo messaggio.<br>
+Grazie ancora e benvenuto a nome di tutta l'associazione!<br>
+Cordiali saluti,<br>
 Il team di ProntoEmrgenza di Agnosine(BS)",
     "oggetto"=>" Grazie per esserti registrato su Pronto Emergenza!!",
 
 ];
-$cambio_psw=[
+$cambioPsw=[
     "titolo"=>"Gentile {nomeUtente},",
-    "testo"=>"La tua password è stata cambiata con successo.
+    "testo"=>"La tua password è stata cambiata con successo.<br>
 Se hai richiesto tu questo cambio password, ora puoi accedere al tuo account con la nuova password. 
-Se non hai richiesto questo cambio o sospetti un accesso non autorizzato, ti invitiamo a contattare il nostro team di supporto all'indirizzo [indirizzo email del supporto] o al numero [numero di telefono del supporto].
-Grazie,
-Pronto Emergenza
-Associazione di volontariato",
+Se non hai richiesto questo cambio o sospetti un accesso non autorizzato, ti invitiamo a contattare il nostro team di supporto all'indirizzo info@prontoemergenza.it o al numero 0365/826210.<br>
+Grazie,<br>
+Il team di ProntoEmrgenza di Agnosine(BS)<br>
+Associazione di Volontariato",
     "oggetto"=>"Conferma cambio password"
 ];
 $nuovomeseProgrammazione=[
