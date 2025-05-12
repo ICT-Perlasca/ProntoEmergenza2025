50dd441430b1967356ad09c976479d379ca59f85
ManuelVersini <versinimanuel2005@gmail.com>
2025-05-12 13:44:30 +0200

    Problemi aggancio bootstrap
    Problemi aggancio bootstrap

--- pages/click_email.php
+++ pages/click_email.php
@@ -1,42 +1,45 @@
 <?php
-require_once "../funzioniDB.php";
+require '../funzioniDB.php';
+require '../components/Head/head.php';
 
-$idU = $_GET['id'];
-$data = $_GET['data'];
-$idUHash = $_GET['idUHash'];
-$dataHash = $_GET['dataHash'];
+//$idU = $_GET['id'];
+//$data = $_GET['data'];
+//$idUHash = $_GET['idUHash'];
+//$dataHash = $_GET['dataHash'];
 
+$idU = 1;
+$data = '2025-03-10 00:00:00';
+$idUHash = md5($idU);
+$dataHash = md5($data);
 ?>
 <html>
-<head>
-    <link href='./public/css/bootstrap.min.css' rel='stylesheet'/>
-    <script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js'></script>
-    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js'></script>
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'>
-</head>
+<?php
+    echo COMP_head(); 
+?>
 <body>
 <?php
 if ($idUHash === md5($idU) && $dataHash === md5($data)) {
-    $dataora = new DateTime('now', new DateTimeZone('Europe/Rome'));
-    $dataoraStr = $dataora->format("Y-m-d H:i:s");
+    //$dataora = new DateTime('now', new DateTimeZone('Europe/Rome'));
+    $dataora = new DateTime('2025-03-10 12:59:59');
+    $dataoraStr = $dataora->format('Y-m-d H:i:s');
 
-    $query = "SELECT dataoraInvioEmail FROM Utenti WHERE idUtente = ? AND dataInvioEmail = ?";
+    $query = 'SELECT dataoraInvioEmail FROM Utenti WHERE idUtente = ? AND dataoraInvioEmail = ?';
     $valori = [$idU, $data];
-    $tipi = ["PDO::PARAM_INT", "PDO::PARAM_STR"];
+    $tipi = [PDO::PARAM_INT, PDO::PARAM_STR];
     $dataInvio = db_query($query, $valori, $tipi);
 
-    if (!isset($dataInvio['errore'])) {
+    if (!isset($dataInvio['error'])) {
         if (!empty($dataInvio)) {
+            
             $dataI = new DateTime($dataInvio[0]['dataoraInvioEmail']);
             $diff = $dataora->diff($dataI);
             $oreDifferenza = ($diff->d * 24) + $diff->h;
 
             if ($oreDifferenza < 24) {
-                $query = "UPDATE Utenti SET dataoraClickEmail = ? WHERE idUtente = ?";
+                $query = 'UPDATE Utenti SET dataoraClickEmail = ? WHERE idUtente = ?';
                 $valori = [$dataoraStr, $idU];
-                $tipi = ["PDO::PARAM_STR", "PDO::PARAM_INT"];
+                $tipi = [PDO::PARAM_STR, PDO::PARAM_INT];
                 $ris = db_query($query, $valori, $tipi);
-
                 if ($ris == 1) {
                     echo "<div class='alert alert-success' role='alert'>
                         <h4 class='alert-heading'>Ben Fatto!</h4>
