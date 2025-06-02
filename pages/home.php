cea742b3d277216a5678e1663e8561e96ea980d6
claudiaprt <prati.cld@gmail.com>
2025-06-02 22:44:20 +0200

    sistemazione link home page

--- pages/home.php
+++ pages/home.php
@@ -34,9 +34,9 @@ if(!isset($_SESSION['idUtente'])){
             <div class="container mt-4">
                 <div class="d-flex flex-wrap justify-content-center gap-2">
                     <?php
-                        echo COMP_Button('person-circle','Profilo','profiloUtente');
-                        echo COMP_Button('calendar-date','Calendario','turni');
-                        echo COMP_Button('card-list','Bacheca','bacheca');
+                        echo COMP_Button('person-circle','Profilo','utenteSingolo');
+                        echo COMP_Button('calendar-date','Calendario','turni/elenco');
+                        echo COMP_Button('card-list','Bacheca','bacheca/elenco');
                         if($_SESSION['tipoUtente'] == 'admin')
                             echo COMP_Button('people-fill','Gestione Utenti','utenti/profili');
                     ?>
