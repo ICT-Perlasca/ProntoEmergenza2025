cc77f5fb56e2556326820adf4ba1e1c510875767
Gallina Roberto <gallinar00@gmail.com>
2025-06-04 10:49:55 +0200

    Fix inserimento comunicazione

--- api/apiINScom.php
+++ api/apiINScom.php
@@ -6,23 +6,17 @@ function API_AggiuntaComunicazione($get, $post, $session){
     echo "Controllo immagine: " . $ctrlImg . "\n";
     if($ctrlImg == false)
     {
-        echo json_encode(["errore" => "file non compatibile"]);
-        return;       
+        return ["errore" => "file non compatibile"];
     }
     if (!isset($session['tipoUtente']) || $session['tipoUtente'] != "admin"){
         header('HTTP/1.1 403 Forbidden');
-        echo json_encode(["errore" => "Accesso negato"]);
-        return;
+        return  ["errore" => "Accesso negato"];
     }
 
     if (!isset($post['titolo'], $post['descrizione'], $post['data_scadenza'], $post['tipo'])) {
         header('HTTP/1.1 400 Bad Request');
-        echo json_encode(["errore" => "Dati mancanti"]);
-        return;
+        return ["errore" => "Dati mancanti"];
     }
-
-
-
 
     $sql = "INSERT INTO comunicazioni (titolo, testo, dataScadenza, idTipo, dataEmissione, nomeFileAllegato, idUtente) 
             VALUES (?, ?, ?, ?, ?, ?, ?)";
@@ -50,10 +44,10 @@ function API_AggiuntaComunicazione($get, $post, $session){
     $risposta = db_query($sql, $valori, $tipi);
 
     if ($risposta) {
-        echo json_encode(["successo" => true]);
+        return ["successo" => true];
     } else {
         header('HTTP/1.1 500 Internal Server Error');
-        echo json_encode(["errore" => "Errore nel database"]);
+        return ["errore" => "Errore nel database"];
     }
 }
 ?>
