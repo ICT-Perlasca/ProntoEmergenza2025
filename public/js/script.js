6693afa12ab44aed4aae52b8a47002331545d7c3
Gallina Roberto <gallinar00@gmail.com>
2025-06-04 16:44:19 +0200

    Remove not used file

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-function aggiungiAlert(messaggio, idcontenitore) {
-    let alert = document.getElementById(idcontenitore);
-    alert.innerHTML = `
-        <div class="alert alert-info alert-dismissible fade show text-center" role="alert">
-            ${messaggio}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    `;
-}
