c45c95dfb948f19eb1dfe27aa187a0e16d930496
Gallina Roberto <gallinar00@gmail.com>
2025-06-04 16:32:57 +0200

    Improve graphics

--- components/SimpleComponent/searchBar.php
+++ components/SimpleComponent/searchBar.php
@@ -9,9 +9,12 @@
 			$nome="targaNome";
 			$place="Inserisci la Targa o il Nome del mezzo";
 		}
-		return "
-			<div class='col-9 m-auto pt-3'><input class='form-control' type=text name='$nome' placeholder='$place' onInput='filtra(this)'></div>
-			<script src='.\public\js\searchBar.js'></script>";
+		return '
+			<div class="input-group col-6 m-auto pt-3">
+				<input class="form-control" type=text name="'.$nome.'" placeholder="'.$place.'" onInput="filtra(this)">
+				<span class="input-group-text rounded-right" style="border-radius:0px"><i class="bi bi-search"></i></span>
+			</div>
+			<script src=".\public\js\searchBar.js"></script>';
 	}
 ?>
 
