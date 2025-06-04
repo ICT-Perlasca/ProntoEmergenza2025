f3123d3d79a275c41ca5e3e79b195cc2b3305a32
ElKev05 <rumikevin2005@gmail.com>
2025-06-04 10:22:18 +0200

    api aggiunte al file api.php

--- api.php
+++ api.php
@@ -25,7 +25,39 @@ switch ($route) {
     case 'api/RitornaUtenti':
         require_once ("./api/RitornaUtenti.php");
         echo json_encode(API_RitornaUtenti($_GET, $_POST, $_SESSION));
-        break;            
+        break;
+    case 'api/API_ElencoCompleanni':
+        require_once ("./api/API_ElencoCompleanni.php");
+        echo json_encode(API_ElencoCompleanni($_GET, $_POST, $_SESSION));
+        break;
+    case 'api/API_ElencoEventiPrevisti':
+        require_once ("./api/API_ElencoEventiPrevisti.php");
+        echo json_encode(API_ElencoEventiPrevisti($_GET, $_POST, $_SESSION));
+        break;
+    case 'api/API_ElencoOreUtente':
+        require_once ("./api/API_ElencoOreUtente.php");
+        echo json_encode(API_ElencoOreUtente($_GET, $_POST, $_SESSION));
+        break;
+    case 'api/API_ElencoOreUtenteMensile':
+        require_once ("./api/API_ElencoOreUtenteMensile.php");
+        echo json_encode(API_ElencoOreUtenteMensile($_GET, $_POST, $_SESSION));
+        break;  
+    case 'api/API_ElencoTurniAnnoPrecedente':
+        require_once ("./api/API_ElencoTurniAnnoPrecedente.php");
+        echo json_encode(API_ElencoTurniAnnoPrecedente($_GET, $_POST, $_SESSION));
+        break;
+    case 'api/API_ElencoTurniConvalidatiMensile':
+        require_once ("./api/API_ElencoTurniConvalidatiMensile.php");
+        echo json_encode(API_ElencoTurniConvalidatiMensile($_GET, $_POST, $_SESSION));
+        break;
+    case 'api/API_ElencoTurniConvalidatiUtente':
+        require_once ("./api/API_ElencoTurniConvalidatiUtente.php");
+        echo json_encode(API_ElencoTurniConvalidatiUtente($_GET, $_POST, $_SESSION));
+        break;
+    case 'api/API_ritornaTurnoUtente_tra_due_date':
+        require_once ("./api/API_ritornaTurnoUtente_tra_due_date.php");
+        echo json_encode(API_ritornaTurnoUtente_tra_due_date($_GET, $_POST, $_SESSION));
+        break;
 
     default:
         http_response_code(404);
