d377ff953dba2899140fc2f33e4c8eaa3db1da86
DavideTogni06 <davide.togni2608@gmail.com>
2025-06-06 19:31:03 +0200

    Add files via upload

--- public/js/VisualizzaTurniUtente.js
+++ public/js/VisualizzaTurniUtente.js
@@ -8,3 +8,19 @@ function getTurni(form){
     }
     xhttp.send("utente="+utente);
 }
+
+function stampaTurni(xhttp){
+    let ris = JSON.parse(xhttp.responseText);
+    if(ris == null)
+       document.getElementById('ris').innerHTML = "Errore connessione";
+    else if(ris.length == 0)
+       document.getElementById('ris').innerHTML = "Nessuna turno trovato";
+    else{
+        let tab = "<table><tr><th>idTurnoUtente</th><th>testoNota</th<th>oraInizioEffettiva</th><th>oraInizioEffettiva</th>></tr>";
+        for(let i=0; i<ris.length; i++){
+            tab += "<tr><td>"+ris[i].idTurnoUtente+"</td><td>"+ris[i].testoNota+"</td><td>"+ris[i].oraInizioEffettiva+"</td><td>"+ris[i].oraInizioEffettiva+"</td></tr>";
+        }
+        tab += "</table>";
+        document.getElementById('ris').innerHTML = tab;
+    }
+}
