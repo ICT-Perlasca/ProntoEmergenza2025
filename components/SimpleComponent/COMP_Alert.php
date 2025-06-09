60d78c2f19f21ef8935978bdc1d1c4ddbe3cd4aa
claudiaprt <prati.cld@gmail.com>
2025-06-09 18:59:51 +0200

    sistemazione visualizzazione profilo utente loggato

---
+++ components/SimpleComponent/COMP_Alert.php
@@ -0,0 +1,6 @@
+<?php
+function COMP_Button($$message) {
+    $codice="<div class='col-sm-6 col-md-3 text-center alert-secondary' role='alert'>$message</div>";
+    return $codice;
+}
+?>
--- pages/utenti/profiloUtente.php
+++ pages/utenti/profiloUtente.php
@@ -1,39 +1,40 @@
 <?php
 session_start();
-if(!isset($_SESSION['idUtente'])){
+if(!isset($_SESSION['idUtente']))
     header("Location: login");
-}else{
-	
-    if (isset($_GET['emailUt']) || isset($_SESSION['email'])){
-        
+else{
+    if (isset($_GET['emailUt']) || isset($_SESSION['email']))
+    {
 ?>
-<html>
+        <html>
+        <?php
+        require_once ("./components/Head/head.php");
+        echo COMP_head();
+        ?>
+        <body>
+        <?php
+        require_once ("./components/Header/header.php");
+        require_once ("./components/Footer/footer.php");
+        require_once ("./components/profiloUtenteSingolo/COMP_profiloUtenteSingolo.php");
+
+        echo COMP_header($_SESSION);
+
+        if(isset($_GET['emailUt']))
+            $email=$_GET['emailUt'];
+        else
+            $email=$_SESSION['email'];
+
+        $codice=CMP_profiloUtenteSingolo($email);
+        if ($codice=="")
+            echo COMP_Alert("utente non autorizzato a vedere il profilo");
+        else
+            echo $codice;
+
+        echo COMP_Footer();
+        ?>
+            </body>
+        </html>
 <?php
-require_once ("./components/Head/head.php");
-echo COMP_head();
-?>
-    <body>
-<?php
-require_once ("./components/Header/header.php");
-require_once ("./components/Footer/footer.php");
-require_once ("./components/profiloUtenteSingolo/COMP_profiloUtenteSingolo.php");
-
-echo COMP_header($_SESSION);
-
-
-
-if(isset($_GET['emailUt'])){
-	echo CMP_profiloUtenteSingolo($_GET['emailUt']);
-}else{
-	echo CMP_profiloUtenteSingolo($_SESSION['email']);
-}
-
-echo COMP_Footer();
-?>
-    </body>
-</html>
-<?php
-    }
-    
+    }  
 }
 ?>
